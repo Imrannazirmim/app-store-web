7b546686126f319dcd2362ae62a30673571948ef
Imrannazirmim <imrannazirmim22@gmail.com>
2025-10-09 22:59:25 +0600

    readme file update

--- README.md
+++ README.md
@@ -1,16 +1,56 @@
-# React + Vite
+### GetStoreApp
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This modern web site build with react, taiwindcss or react router. This website features user explore, search, view app, and defferent application install.
 
-Currently, two official plugins are available:
+### Features
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) (or [oxc](https://oxc.rs) when used in [rolldown-vite](https://vite.dev/guide/rolldown)) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Live Search
+- Responsive Design
+- Dynamic Data Fetching
+- Clean or Modern UI
+- Sorting app Card
+- Build Rechart
+- Dynamic Install Card
 
-## React Compiler
+### Technologies Used
 
-The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
+- React
+- React Router
+- Tailwind css
+- React toastify
+- Rechart
+- **Build Tool** | Vite |
+- **Icons** | React Ions |
 
-## Expanding the ESLint configuration
+### Installation && Run
 
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+1. **Clone Repository**
+
+```base
+  https://github.com/Imrannazirmim/app-store-web.git
+```
+
+2. **Currect Project Directory**
+
+```base
+cd app-store
+```
+
+3. **Install dependencies**
+
+```base
+npm install
+
+```
+
+4. **Start server**
+
+```base
+npm run dev
+```
+
+5. **Open Brower**
+
+```arduino
+http://localhost:5173
+```
--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -1,10 +1,14 @@
-import useStoreFetch from "../Hooks/useStoreFetch.jsx";
-import Loading from "../Components/Ui/Loading.jsx";
+import { useState } from "react";
+import { CiSearch } from "react-icons/ci";
 import AppCard from "../Components/Layout/AppCard.jsx";
+import Loading from "../Components/Ui/Loading.jsx";
+import useStoreFetch from "../Hooks/useStoreFetch.jsx";
+import errorImg from "../assets/App-Error.png";
 
 const url = `/store.json`;
 const Apps = () => {
   const { isLoading, error, appsData } = useStoreFetch(url);
+  const [searchTerm, setSearchTerm] = useState("");
   if (isLoading) {
     return (
       <div>
@@ -13,6 +17,11 @@ const Apps = () => {
     );
   }
   if (error) return <span>Failed loading Data. Please try again.</span>;
+
+  // search functionality
+  const searchApps = appsData.filter((app) =>
+    app.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <section className="px-20 py-10 bg-[#E9E9E9] ">
       <div className="flex flex-col gap-2 text-center py-20">
@@ -23,10 +32,36 @@ const Apps = () => {
           Explore All Apps on the Market developed by us. We code for Millions
         </p>
       </div>
+      <div className="flex justify-between py-8 ">
+        <h2 className="text-xl">
+          <strong className="text-purple-500">({appsData.length})</strong> Apps
+          Found
+        </h2>
+        <div className="relative  max-w-sm px-4 sm:px-0">
+          <CiSearch className="absolute left-0 top-1/2 z-1 ml-2  -translate-y-1/2 font-semibold text-lg" />
+
+          <input
+            type="text"
+            className="py-2 px-8 bg-white rounded-md  text-black relative"
+            placeholder="Search Apps"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </div>
       <div className="gird-card ">
-        {appsData?.map((app) => (
-          <AppCard key={app.id} app={app} />
-        ))}
+        {searchApps.length > 0 ? (
+          searchApps?.map((app) => <AppCard key={app.id} app={app} />)
+        ) : (
+          <section className="container mx-auto">
+            <div className="w-full h-screen container flex flex-col mx-auto text-center gap-4">
+              <img src={errorImg} alt="error image" />
+              <h2 className="text-3xl font-semibold text-center">
+                Oops, Apps not found!
+              </h2>
+            </div>
+          </section>
+        )}
       </div>
     </section>
   );
